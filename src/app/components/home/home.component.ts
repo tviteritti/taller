948cd0d8fd62a9a430f31995deaa6f8ac07bd54d
focho27 <gerorod1999@gmail.com>
2021-11-20 20:43:34 -0300

    comunicacion back con front

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,7 +1,7 @@
 
 import { Component, OnInit } from '@angular/core';
-
-
+import { ProductoService, IProducts } from '../../SERVICES/producto.service'
+import {Router } from '@angular/router';
 
 
 
@@ -9,13 +9,26 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-home',
   templateUrl: './home.component.html'
 })
+
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
+ products:any = [];
+  constructor(private ProductoService:ProductoService, private router:Router) { }
+ 
   ngOnInit(): void {
   }
 
 
-
+   obtener():any {
+     console.log("llegue component front")
+     this.products= this.ProductoService.obtener().subscribe(data => {   // data is already a JSON object
+      console.log(data);
+      this.obtenerJson(data)
+      return JSON.stringify(data);
+  });
+    console.log(this.products);
+  }
+  obtenerJson(products:any){
+   console.log(products);
+  }
+  
 }
