afe3c576df9ad2a37460ed3ecf08a9566d099298
focho27 <gerorod1999@gmail.com>
2021-11-20 22:07:10 -0300

    finalizado

--- api/app.js
+++ api/app.js
@@ -12,13 +12,20 @@ app.set('port', port);
 
 app.use('/api', require('./rutas'))
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 
 
 
 app.listen(app.get('port'), (error) => {
     if (error) {
         console.log('error' + error);
-
+        return;
     } else {
         console.log('Example app listening on port ' + port)
 
