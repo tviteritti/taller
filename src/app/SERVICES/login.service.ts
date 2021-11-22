17fdf2369f41c004852bd4adee848d2484eb3687
Tomas <tviteritti@alumno.unlam.edu.ar>
2021-11-22 09:19:51 -0300

    cambio de nombre login service

--- src/app/SERVICES/login.service.ts
+++ src/app/SERVICES/login.service.ts
@@ -4,7 +4,7 @@ import{HttpClient} from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
-export class EquipoService {
+export class LoginService {
 
   url= '/api/login';
   constructor(private http: HttpClient) { }
