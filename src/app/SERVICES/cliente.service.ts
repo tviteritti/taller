06a03e19ac7372017d55d139da578c82f60c66b1
Tomas <tviteritti@alumno.unlam.edu.ar>
2021-11-23 12:50:58 -0300

    registro en bd local

--- src/app/SERVICES/cliente.service.ts
+++ src/app/SERVICES/cliente.service.ts
@@ -7,10 +7,15 @@ import { HttpClient } from '@angular/common/http';
 export class ClienteService {
 
   url = '/api/obteberIdcliente';
+  url2 = '/api/registro';
 
   constructor(private http: HttpClient) { }
 
     public obtenerId(email: string) {
     return this.http.post(this.url,{email: email,});
+  }
+  public insertar(nombre: string, contraseña: string, apellido: string, direccion: string, email: string) {
+      console.log("1")
+    return this.http.post(this.url2,{nombre: nombre,contraseña: contraseña,apellido: apellido,direccion: direccion,email: email});
   }
 }
